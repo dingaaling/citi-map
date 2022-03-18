799d1ad8e2d2cbfd474fbb97246420ba328257be
Christopher Arias <87148213+ariaslabs@users.noreply.github.com>
2022-03-18 08:00:17 -0700

    Converted files from Class to Hook (#10)

--- src/PathMap.js
+++ src/PathMap.js
@@ -4,47 +4,40 @@ import Grid from '@material-ui/core/Grid';
 import {  Map, TileLayer, Marker } from 'react-leaflet'
 import { icon1, icon2, icon3 } from './icon';
 
-class PathMap extends React.Component {
+const PathMap = (props) => {
 
-render(){
-    return (
-      <div className="leaflet-container">
+  return (
+    <div className="leaflet-container">
+      <Grid container item xs={12} spacing={1}>
+        <Grid item xs={2}></Grid>
+        <Grid item xs={8}>
+          <Map center={props.mapCenter} zoom={15}>
+          <TileLayer
+            attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          />
 
-        <Grid container item xs={12} spacing={1}>
-          <Grid item xs={2}></Grid>
-          <Grid item xs={8}>
-                <Map center={this.props.mapCenter} zoom={15}>
-                <TileLayer
-                  attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-                  url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
-                />
+          {props.icon1List.map((position, idx) =>
+            <Marker key={`marker-${idx}`} position={position} icon={ icon1 }>
+            </Marker>
+          )}
 
-                {this.props.icon1List.map((position, idx) =>
-                  <Marker key={`marker-${idx}`} position={position} icon={ icon1 }>
-                  </Marker>
-                )}
+          {props.icon2List.map((position, idx) =>
+            <Marker key={`marker-${idx}`} position={position} icon={ icon2 }>
+            </Marker>
+          )}
 
-                {this.props.icon2List.map((position, idx) =>
-                  <Marker key={`marker-${idx}`} position={position} icon={ icon2 }>
-                  </Marker>
-                )}
+          {props.icon3List.map((position, idx) =>
+            <Marker key={`marker-${idx}`} position={position} icon={ icon3 }>
+            </Marker>
+          )}
 
-                {this.props.icon3List.map((position, idx) =>
-                  <Marker key={`marker-${idx}`} position={position} icon={ icon3 }>
-                  </Marker>
-                )}
-
-
-                </Map>
-          </Grid>
-          <Grid item xs={2}></Grid>
-
+          </Map>
         </Grid>
-        </div>
-    );
-
-}
-
+        <Grid item xs={2}></Grid>
+      </Grid>
+    </div>
+  )
 }
 
 export default PathMap;
